87b81df0449d97d3213cf6259a94e7102e82c8a2
WebbKim <acebee2000@gmail.com>
2015-10-19 15:24:02 +0900

    myapp

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(3000, function(){
-  console.log('Server On!');
+    console.log('Server On!');
 });
